f6367f08f5dda6aa0c67fdc700c5aad46e0ae8a2
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-07-01 15:50:44 +0300

    init commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <img src="" alt="logo">
+    </div>
+    <div class="header__top">
+
+      <div class="header__date">
+      </div>
+      <div class="header__search">
+        <input>
+      </div>
+    </div>
+    <nav class="header__categories categories">
+      <ul class="categories__list">
+        <li class="categories__item">
+          <button class="categories__item-button">Categories</button>
+        </li>
+        <li class="categories__item">
+          <button class="categories__item-button">All</button>
+        </li>
+        <li class="categories__item">
+          <button class="categories__item-button">Water</button>
+        </li>
+        <li class="categories__item">
+          <button class="categories__item-button">Coffee</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
