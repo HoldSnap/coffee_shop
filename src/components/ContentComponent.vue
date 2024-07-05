cd268651991b3b7caa4dacf488f50dcf7a4cf08f
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-07-05 19:01:27 +0300

    fix: change assets

--- src/components/ContentComponent.vue
+++ src/components/ContentComponent.vue
@@ -4,25 +4,10 @@
   <section class="main_content content">
     <ul class="content__list">
       <li class="content__item">
-        <img class="content__item-img" src="../assets/" alt="water" />
+        <img class="content__item-img" src="" alt="water" />
         <div class="content__item_prise"></div>
       </li>
-      <li class="content__item">
-        <img class="content__item-img" src="../assets/" alt="water" />
-        <div class="content__item_prise"></div>
-      </li>
-      <li class="content__item">
-        <img class="content__item_prise" src="../assets/" alt="water" />
-        <div class="content__item_prise"></div>
-      </li>
-      <li class="content__item">
-        <img class="content__item-img" src="../assets/" alt="water" />
-        <div class="content__item_prise"></div>
-      </li>
-      <li class="content__item">
-        <img class="content__item-img" src="../assets/" alt="water" />
-        <div class="content__item_prise"></div>
-      </li>
+
     </ul>
   </section>
 </template>
